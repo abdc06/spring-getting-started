0a8b79c11ee7994900c846baba8768bf8b2377dc
abdc06 <abdc2806@gmail.com>
2022-10-13 23:52:46 +0900

    스케줄 샘플 코드 추가

--- scheduling-tasks/src/main/java/me/abdc/schedulingtasks/SchedulingTasksApplication.java
+++ scheduling-tasks/src/main/java/me/abdc/schedulingtasks/SchedulingTasksApplication.java
@@ -2,8 +2,10 @@ package me.abdc.schedulingtasks;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class SchedulingTasksApplication {
 
     public static void main(String[] args) {
