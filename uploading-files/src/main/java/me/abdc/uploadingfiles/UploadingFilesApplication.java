94c8081d1abd9b32573d3eb3aef575745795389d
abdc06 <abdc2806@gmail.com>
2022-10-14 12:05:29 +0900

    uploading-files 프로젝트 생성

---
+++ uploading-files/src/main/java/me/abdc/uploadingfiles/UploadingFilesApplication.java
@@ -0,0 +1,13 @@
+package me.abdc.uploadingfiles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UploadingFilesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UploadingFilesApplication.class, args);
+    }
+
+}
