0e272b167ee8363459d627f3f1dbbba760dd50a5
abdc06 <abdc2806@gmail.com>
2022-10-15 14:04:43 +0900

    validating-form-input 프로젝트 생성

---
+++ validating-form-input/src/test/java/me/abdc/validatingforminput/ValidatingFormInputApplicationTests.java
@@ -0,0 +1,13 @@
+package me.abdc.validatingforminput;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValidatingFormInputApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
